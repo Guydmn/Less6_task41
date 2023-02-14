2e494bad08bbe254a3e974c79d57c4eb2188dd17
Roman Guydmn <romanguydmn@gmail.com>
2023-02-14 12:09:51 +0400

    Задача 41 Семинар 6

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+
+Console.Write("Введите количество чисел в массиве: ");
+int elementsCount = int.Parse(Console.ReadLine());
+int[] myArray = new int[elementsCount];
+
+Console.WriteLine("Вывод массива: ");
+
+for (int i = 0; i < myArray.Length; i++)
+{
+    Console.Write($"Введите элемент массива с индексом {i}: ");
+    myArray[i] = int.Parse(Console.ReadLine());
+}
+    Console.WriteLine($"Вы ввели массив: ");
+    for (int i = 0; i < myArray.Length; i++)
+    {
+        Console.Write($"{myArray[i]} ");
+    }
+Console.WriteLine();
+
+    int count = 0;
+for (int i = 0; i < myArray.Length; i++)
+{
+    if (myArray[i] > 0)
+    {
+        count = count+1;
+    }
+}
+    Console.WriteLine($"Количество чисел в массиве > 0 равно {count}");
